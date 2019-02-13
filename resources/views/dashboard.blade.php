838d9c07d72399e37d473601665409a035e3769b
Ahmed <ahmedyarub@yahoo.com>
2019-02-13 19:15:35 +0400

    Add user name to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,15 +25,10 @@
             <div class="col-md-6 col-xl-5">
                 <div class="block">
                     <div class="block-header">
-                        <h3 class="block-title">Welcome to your app</h3>
+                        <h3 class="block-title">Welcome {{Auth::user()->name}}</h3>
                     </div>
                     <div class="block-content">
-                        <p class="font-size-sm text-muted">
-                            We’ve put everything together, so you can start working on your Laravel project as soon as possible! OneUI assets are integrated and work seamlessly with Laravel Mix, so you can use the npm scripts as you would in any other Laravel project.
-                        </p>
-                        <p class="font-size-sm text-muted">
-                            Feel free to use any examples you like from the full versions to build your own pages. <strong>Wish you all the best and happy coding!</strong>
-                        </p>
+
                     </div>
                 </div>
             </div>
